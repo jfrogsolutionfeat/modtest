8e8ff51e480a2ffc9103ca79af5dd8bd0d162a87
jfrogsolutionfeat <jfrog-solutions-ci@jfrog.com>
2019-10-18 15:11:20 -0700

    commit with no tags

---
+++ go-1-12/jfrogsolutionfeatModFile
@@ -0,0 +1,7 @@
+module github.com/jfrogsolutionfeat/modtest
+
+require (
+    github.com/jfrog-solutiontest/employee v1.1.0
+    github.com/kr/pty v1.1.3
+    github.com/jfrog-solutiontest/notexist v1.0.0
+)
---
+++ modtest.go
@@ -0,0 +1,11 @@
+package modtest
+
+import (
+  "fmt"
+  "github.com/kr/pty"
+  "github.com/jfrog-solutiontest/employee"
+)
+
+func ModuleTestPackageName () {
+   fmt.Println("Package Name: Module Test Package Test")
+}
